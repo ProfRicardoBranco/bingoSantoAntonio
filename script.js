f39eb301fb905dbfee2af79ee6958505ca3f5fcc
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-22 14:28:57 -0300

    segundo site

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const numeroSorteado = document.getElementById('numeroSorteado');
+const bingoTable = document.getElementById('bingoTable');
 const sortearBtn = document.getElementById('sortearBtn');
+let numerosSorteados = [];
+
+// Preencher a tabela com os números de 1 a 100
+for (let i = 1; i <= 100; i++) {
+    const cell = document.createElement('td');
+    cell.textContent = i;
+    bingoTable.appendChild(cell);
+}
 
 sortearBtn.addEventListener('click', () => {
-    const numero = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório de 1 a 100
-    numeroSorteado.textContent = `Número sorteado: ${numero}`;
+    if (numerosSorteados.length === 100) {
+        alert('Todos os números já foram sorteados!');
+        return;
+    }
+
+    let numeroSorteado;
+    
+    do {
+        numeroSorteado = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório de 1 a 100
+    } while (numerosSorteados.includes(numeroSorteado));
+
+    numerosSorteados.push(numeroSorteado);
+    
+    const cellIndex = numeroSorteado - 1; // Índice da célula na tabela
+    const cell = bingoTable.getElementsByTagName('td')[cellIndex];
+    
+    cell.style.backgroundColor = '#007bff';
 });
