25d6a9a14f67708541993c13904064d514e27b51
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-22 16:32:40 -0300

    asd

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const bingoTable = document.getElementById('bingoTable');
-const sortearBtn = document.getElementById('sortearBtn');
-let numerosSorteados = [];
-
-// Preencher a tabela com os números de 1 a 100 em uma grade 10x10
-for (let i = 1; i <= 100; i++) {
-    if ((i - 1) % 10 === 0) {
-        var row = bingoTable.insertRow();
-    }
-    const cell = row.insertCell();
-    cell.textContent = i;
-    cell.style.padding = '5px 10px'; // Adiciona espaçamento interno
-    cell.style.fontSize = '40px'; // Define o tamanho da fonte como 40
-    cell.style.fontWeight = 'bold'; // Define a fonte como negrito
-}
-
 sortearBtn.addEventListener('click', () => {
     if (numerosSorteados.length === 100) {
         alert('Todos os números já foram sorteados!');
@@ -37,5 +21,8 @@ sortearBtn.addEventListener('click', () => {
     cell.classList.add('marked');
     cell.style.backgroundColor = '#28a745'; // Altera a cor de fundo da célula
 
-    alert(`Número sorteado: ${numeroSorteado}`);
+    const enviarAlerta = document.getElementById('enviarAlerta').checked; // Verifica se o seletor está marcado
+    if (enviarAlerta) {
+        alert(`Número sorteado: ${numeroSorteado}`);
+    }
 });
