86d29cbb23f3a24665f24506d18e475fc3a08ba4
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-03-24 18:57:53 -0300

    asdf

--- script.js
+++ script.js
@@ -47,12 +47,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     novoJogoBtn.addEventListener('click', () => {
         maximoBingo = parseInt(maximoBingoInput.value);
-        alert('Botão Novo Jogo clicado');
-        reiniciarJogo(maximoBingo);
-        numerosSorteados = new Set(); // Reinicia o conjunto de números sorteados
+        reiniciarJogo();
+        numerosSorteados.clear(); // Limpa o conjunto de números sorteados
     });
+    
+});
 
+novoJogoBtn.addEventListener('click', () => {
 
+    reiniciarJogo();
+    numerosSorteados.clear(); // Limpa o conjunto de números sorteados
+});
 
 // Função para limpar a tabela e reiniciar o jogo
 function reiniciarJogo(maximoBingo) {
